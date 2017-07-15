43dc8fb8b5b9c7f67748ca4b18f6dc912fdbabaf
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2017-07-14 21:15:39 -0400

    Mumuki Export on 2017-07-14 21:15:39 -0400

--- 00001_Algo complejo.../test.cpp
+++ 00001_Algo complejo.../test.cpp
@@ -1,2 +1,19 @@
 class MumukiTest : public CppUnit::TestFixture  {
+  CPPUNIT_TEST_SUITE( MumukiTest );
+    CPPUNIT_TEST( testComplejoTieneComponenteReal );
+    CPPUNIT_TEST( testComplejoTieneComponenteImaginaria );
+  CPPUNIT_TEST_SUITE_END();
+
+  void testComplejoTieneComponenteReal() {
+    Complejo * complejo = new Complejo();
+    complejo->real = 1;
+    CPPUNIT_ASSERT( complejo->real == 1 );
+  }
+  
+  void testComplejoTieneComponenteImaginaria() {
+    Complejo * complejo = new Complejo();
+    complejo->imaginario = 1;
+    CPPUNIT_ASSERT( complejo->imaginario == 1 );
+  }
+  
 };
